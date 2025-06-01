3866b65bd78c65b5bd91d08dc0c510eb46baa65a
缪心雨 <1286280961@qq.com>
2025-06-01 21:35:57 +0800

    maku低代码开发平台搭建

---
+++ maku-framework/src/main/java/net/maku/framework/security/user/SecurityUser.java
@@ -0,0 +1,51 @@
+package net.maku.framework.security.user;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * 用户
+ *
+ * @author 阿沐 babamu@126.com
+ * <a href="https://maku.net">MAKU</a>
+ */
+public class SecurityUser {
+
+    /**
+     * 获取用户信息
+     */
+    public static UserDetail getUser() {
+        UserDetail user;
+        try {
+            user = (UserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Exception e) {
+            return null;
+        }
+
+        return user;
+    }
+
+    /**
+     * 获取用户ID
+     */
+    public static Long getUserId() {
+        UserDetail user = getUser();
+        if (user == null) {
+            return null;
+        }
+
+        return user.getId();
+    }
+
+    /**
+     * 获取机构ID
+     */
+    public static Long getOrgId() {
+        UserDetail user = getUser();
+        if (user == null) {
+            return null;
+        }
+
+        return user.getOrgId();
+    }
+
+}
